14c92b8581c2f23b8a11e86fe68977bd39c71ca7
fmeraut <francois.meraut@hotmail.fr>
2021-08-05 14:59:58 +0200

    first commit

---
+++ src/com/inti/Utilisateur.java
@@ -0,0 +1,38 @@
+package com.inti;
+
+public class Utilisateur {
+
+	private String nom;
+	private String prenom;
+	private int age;
+	public Utilisateur() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Utilisateur(String nom, String prenom, int age) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
